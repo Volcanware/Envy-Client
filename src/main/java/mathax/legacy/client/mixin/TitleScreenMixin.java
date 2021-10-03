e048a7dc48eaf6bcf2fce443a9ab89613421f373
Matejko06 <67696646+Matejko06@users.noreply.github.com>
2021-10-03 20:56:54 +0200

    dum dum moment

--- src/main/java/mathax/legacy/client/mixin/TitleScreenMixin.java
+++ src/main/java/mathax/legacy/client/mixin/TitleScreenMixin.java
@@ -1,7 +1,7 @@
 package mathax.legacy.client.mixin;
 
-import mathax.legacy.client.gui.screens.TitleScreen;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
