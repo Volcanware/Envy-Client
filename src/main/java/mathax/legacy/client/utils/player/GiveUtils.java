4003a64f185c10e0980835749d2ad0074ad0dd15
Matejko06 <67696646+Matejko06@users.noreply.github.com>
2021-11-07 10:56:44 +0100

    Update GiveUtils.java

--- src/main/java/mathax/legacy/client/utils/player/GiveUtils.java
+++ src/main/java/mathax/legacy/client/utils/player/GiveUtils.java
@@ -141,7 +141,24 @@ public class GiveUtils {
                 NbtCompound explosionCompound = new NbtCompound();
                 explosionCompound.putByte("Type", (byte) random.nextInt(5));
 
-                int colors[] = {1973019, 11743532, 3887386, 5320730, 2437522, 8073150, 2651799, 11250603, 4408131, 14188952, 4312372, 14602026, 6719955, 12801229, 15435844, 15790320};
+                int colors[] = {
+                    1973019,
+                    11743532,
+                    3887386,
+                    5320730,
+                    2437522,
+                    8073150,
+                    2651799,
+                    11250603,
+                    4408131,
+                    14188952,
+                    4312372,
+                    14602026,
+                    6719955,
+                    12801229,
+                    15435844,
+                    15790320
+                };
 
                 explosionCompound.putIntArray("Colors", colors);
                 explosionList.add(explosionCompound);
