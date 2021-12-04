027c5a9cd70a7f92b2d57ac704fc36bd29bda925
MineKrypt <minekrypt05@gmail.com>
2021-12-04 13:39:19 +1100

    Rainbow Saturation
    This can look really good, I'm surprised it wasn't added already.
    (Has not been tested but barely anything changed)

--- src/main/java/mathax/legacy/client/systems/modules/render/hud/modules/ActiveModulesHUD.java
+++ src/main/java/mathax/legacy/client/systems/modules/render/hud/modules/ActiveModulesHUD.java
@@ -75,6 +75,16 @@ public class ActiveModulesHUD extends HUDElement {
         .build()
     );
 
+        private final Setting<Double> rainbowSaturation = sgGeneral.add(new DoubleSetting.Builder()
+        .name("rainbow-saturation")
+        .description("Saturation of rainbow color mode.")
+        .defaultValue(1)
+        .sliderRange(0, 1)
+        .decimalPlaces(4)
+        .visible(() -> colorMode.get() == ColorMode.Rainbow)
+        .build()
+    );
+    
     private final Setting<Double> rainbowSpeed = sgGeneral.add(new DoubleSetting.Builder()
         .name("rainbow-speed")
         .description("Rainbow speed of rainbow color mode.")
@@ -180,7 +190,7 @@ public class ActiveModulesHUD extends HUDElement {
         if (colorMode == ColorMode.Random) color = module.color;
         else if (colorMode == ColorMode.Rainbow) {
             rainbowHue2 += rainbowSpread.get();
-            int c = java.awt.Color.HSBtoRGB((float) rainbowHue2, 1, 1);
+            int c = java.awt.Color.HSBtoRGB((float) rainbowHue2, (float) rainbowSaturation.get(), 1);
 
             rainbow.r = Color.toRGBAR(c);
             rainbow.g = Color.toRGBAG(c);
