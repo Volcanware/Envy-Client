07e527a988525dde709a505a55118cf98da673e5
NobreHD <60514067+NobreHD@users.noreply.github.com>
2021-12-30 12:33:28 +0000

    Major Changes To Chat Encryption

--- src/main/java/mathax/legacy/client/systems/modules/chat/ChatEncryption.java
+++ src/main/java/mathax/legacy/client/systems/modules/chat/ChatEncryption.java
@@ -1,5 +1,6 @@
 package mathax.legacy.client.systems.modules.chat;
 
+import io.netty.handler.codec.compression.ZlibEncoder;
 import mathax.legacy.client.MatHaxLegacy;
 import mathax.legacy.client.eventbus.EventHandler;
 import mathax.legacy.client.events.game.ReceiveMessageEvent;
@@ -8,17 +9,20 @@ import mathax.legacy.client.mixin.ChatHudAccessor;
 import mathax.legacy.client.settings.*;
 import mathax.legacy.client.systems.modules.Categories;
 import mathax.legacy.client.systems.modules.Module;
+import mathax.legacy.client.utils.base91.Base91;
 import mathax.legacy.client.utils.render.color.SettingColor;
 import net.minecraft.item.Items;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
-import net.minecraft.text.TextColor;
 
+import java.io.ByteArrayOutputStream;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
-import java.util.Base64;
+import java.util.zip.DeflaterOutputStream;
+import java.util.zip.InflaterOutputStream;
+
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
@@ -92,30 +96,36 @@ public class ChatEncryption extends Module {
         ((ChatHudAccessor) mc.inGameHud.getChatHud()).getMessages().removeIf((message) -> message.getId() == event.id && event.id != 0);
 
         Text message = event.getMessage();
-        if (message.getString().endsWith(suffix.get())) {
+        if (message.getString().endsWith(suffix.get()) && !suffix.get().isEmpty()) {
             String[] msg = message.getString().split(" ",2);
             msg[1] = decrypt(msg[1], customKey.get() ? groupKey.get() : password);
             message = new LiteralText(msg[0] + " " + msg[1]).setStyle(message.getStyle().withColor(getIntFromColor(decryptedColor.get().r, decryptedColor.get().g, decryptedColor.get().b)));
         }
-
         event.setMessage(message);
     }
 
     @EventHandler
     private void onMessageSend(SendMessageEvent event) {
         String message = event.message;
-
-        if (encryptAll.get() || message.startsWith(prefix.get())) {
+        if (suffix.get().isEmpty()) {
+            error("Suffix is empty, not sending.");
+            event.message = null;
+            event.setCancelled(true);
+        } else if (encryptAll.get() || message.startsWith(prefix.get())) {
             if (!encryptAll.get()) message = message.substring(prefix.get().length());
             message = encrypt(message, (customKey.get() ? groupKey.get() : password));
+            if(message.length() > 256) {
+                error("Message is too long, not sending.");
+                event.message = null;
+                event.setCancelled(true);
+            }
         }
-
         event.message = message;
     }
 
     public static void setKey(String myKey) {
         try {
-            MessageDigest sha = MessageDigest.getInstance("SHA-1");
+            MessageDigest sha = MessageDigest.getInstance("SHA-256");
             key = Arrays.copyOf(sha.digest(myKey.getBytes(StandardCharsets.UTF_8)), 16);
             secretKey = new SecretKeySpec(key, "AES");
         } catch (NoSuchAlgorithmException exception) {
@@ -128,7 +138,7 @@ public class ChatEncryption extends Module {
             setKey(secret);
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
             cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-            return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes(StandardCharsets.UTF_8))).replace("=", suffix.get());
+            return Base91.encodeToString(cipher.doFinal(compress(strToEncrypt.getBytes(StandardCharsets.UTF_8)))) + suffix.get();
         } catch (Exception exception) {
             error("Error while encrypting: " + exception);
         }
@@ -141,7 +151,7 @@ public class ChatEncryption extends Module {
             setKey(secret);
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
             cipher.init(Cipher.DECRYPT_MODE, secretKey);
-            return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt.replace(suffix.get(), "="))), StandardCharsets.UTF_8);
+            return new String(decompress(cipher.doFinal(Base91.decode(strToDecrypt.substring(0, strToDecrypt.length() - suffix.get().length())))), StandardCharsets.UTF_8);
         } catch (Exception exception) {
             error("Error while decrypting: " + exception);
         }
@@ -154,5 +164,37 @@ public class ChatEncryption extends Module {
         g = (g << 8) & 0x0000FF00;
         b = b & 0x000000FF;
         return 0xFF000000 | r | g | b;
+    }
+
+    public static byte[] compress(byte[] in) {
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            DeflaterOutputStream defl = new DeflaterOutputStream(out);
+            defl.write(in);
+            defl.flush();
+            defl.close();
+
+            return out.toByteArray();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(150);
+            return null;
+        }
+    }
+
+    public static byte[] decompress(byte[] in) {
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            InflaterOutputStream infl = new InflaterOutputStream(out);
+            infl.write(in);
+            infl.flush();
+            infl.close();
+
+            return out.toByteArray();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(150);
+            return null;
+        }
     }
 }
---
+++ src/main/java/mathax/legacy/client/utils/base91/Base91.java
@@ -0,0 +1,112 @@
+package mathax.legacy.client.utils.base91;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Modified version of Jochaim Henke’s original code from http://base91.sourceforge.net/
+ * <p>
+ * basE91 encoding/decoding routines
+ * <p>
+ * Copyright (c) 2000-2006 Joachim Henke All rights reserved.
+ * <p>
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * <p>
+ * - Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer. - Redistributions in binary
+ * form must reproduce the above copyright notice, this list of conditions and
+ * the following disclaimer in the documentation and/or other materials provided
+ * with the distribution. - Neither the name of Joachim Henke nor the names of
+ * his contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ * <p>
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author Joachim Henke (Original version)
+ * @author Benedikt Waldvogel (Modifications)
+ */
+public class Base91 {
+
+    static final byte[] ENCODING_TABLE;
+    private static final byte[] DECODING_TABLE;
+    static final int BASE;
+    private static final float AVERAGE_ENCODING_RATIO = 1.2297f;
+
+    static {
+        String ts = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&()*+,./:;<=>?@[]^_`{|}~\"";
+        ENCODING_TABLE = ts.getBytes(StandardCharsets.ISO_8859_1);
+        BASE = ENCODING_TABLE.length;
+        assert BASE == 91;
+
+        DECODING_TABLE = new byte[256];
+        for (int i = 0; i < 256; ++i)
+            DECODING_TABLE[i] = -1;
+
+        for (int i = 0; i < BASE; ++i)
+            DECODING_TABLE[ENCODING_TABLE[i]] = (byte) i;
+    }
+
+    public static byte[] encode(byte[] data) {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        Base91OutputStream base91OutputStream = new Base91OutputStream(out);
+        try {
+            base91OutputStream.write(data);
+            base91OutputStream.flush();
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to encode", e);
+        }
+        return out.toByteArray();
+    }
+
+    public static String encodeToString(byte[] data) {
+        return new String(encode(data), StandardCharsets.UTF_8);
+    }
+
+    public static byte[] decode(byte[] data) {
+        int dbq = 0;
+        int dn = 0;
+        int dv = -1;
+
+        int estimatedSize = Math.round(data.length / AVERAGE_ENCODING_RATIO);
+        ByteArrayOutputStream output = new ByteArrayOutputStream(estimatedSize);
+
+        for (int i = 0; i < data.length; ++i) {
+            assert DECODING_TABLE[data[i]] != -1;
+            if (dv == -1)
+                dv = DECODING_TABLE[data[i]];
+            else {
+                dv += DECODING_TABLE[data[i]] * BASE;
+                dbq |= dv << dn;
+                dn += (dv & 8191) > 88 ? 13 : 14;
+                do {
+                    output.write((byte) dbq);
+                    dbq >>= 8;
+                    dn -= 8;
+                } while (dn > 7);
+                dv = -1;
+            }
+        }
+
+        if (dv != -1) {
+            output.write((byte) (dbq | dv << dn));
+        }
+
+        return output.toByteArray();
+    }
+
+    public static byte[] decode(String data) {
+        return decode(data.getBytes(StandardCharsets.US_ASCII));
+    }
+}
