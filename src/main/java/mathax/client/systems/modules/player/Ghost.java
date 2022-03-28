2949a4076377a60999ab0f3310ad27d5b6869d74
Matejko06 <67696646+Matejko06@users.noreply.github.com>
2022-03-28 19:27:57 +0200

    Update and rename GhostMode.java to Ghost.java

--- src/main/java/mathax/client/systems/modules/player/Ghost.java
+++ src/main/java/mathax/client/systems/modules/player/Ghost.java
@@ -13,66 +13,58 @@ import net.minecraft.client.gui.screen.DeathScreen;
 import net.minecraft.item.Items;
 
 /*/--------------------------------------------------------------------------------------------------------------/*/
-/*/ This code was epicly skidded from this meteor addon:                                                         /*/
+/*/ Used from Meteor Rejects                                                                                     /*/
 /*/ https://github.com/AntiCope/meteor-rejects/blob/master/src/main/java/anticope/rejects/modules/GhostMode.java /*/
 /*/--------------------------------------------------------------------------------------------------------------/*/
 
 public class GhostMode extends Module {
+    private boolean active = false;
+
+	private int x = 0;
+	private int z = 0;
 	
-	int x = 0;
-	int z = 0;
+    private final SettingGroup sgGeneral = settings.getDefaultGroup();
 	
-	private final SettingGroup sgGeneral = settings.getDefaultGroup();
-	
-	 private final Setting<Boolean> fullFood = sgGeneral.add(new BoolSetting.Builder()
-		        .name("full-food")
-		        .description("Sets the food level client-side to max so u can sprint.")
-		        .defaultValue(true)
-		        .build()
-		    );
+    // General
+
+    private final Setting<Boolean> fullFood = sgGeneral.add(new BoolSetting.Builder()
+    	.name("full-food")
+    	.description("Sets the food level client-side to max so u can sprint.")
+    	.defaultValue(true)
+    	.build()
+    );
 	 
     public GhostMode() {
     	super(Categories.Player, Items.SKELETON_SKULL, "ghost", "Allows you to move after death.");
     }
-    
-    private boolean active = false;
 
-    @EventHandler
-    private void onTick(final TickEvent.Pre event) {
-    	
-    	if (!active) return;
-        if (mc.player.getHealth() < 1f) mc.player.setHealth(20f);
-        if (fullFood.get() && mc.player.getHungerManager().getFoodLevel() < 20) {
-            mc.player.getHungerManager().setFoodLevel(20);
-        }
+    @Override
+    public void onDeactivate() {
+        active = false;
+        warning("You are no longer in ghost mode.");
     }
-    
+
     @EventHandler
     private void onGameJoin(GameJoinedEvent event) {
         active = false;
     }
-    
-    public void onDeactivate() {
-    	super.onDeactivate();
-        active = false;
-        warning("You are no longer in ghost mode.");
-        /*
-        if (mc.player != null && mc.player.networkHandler != null) {
-            mc.player.requestRespawn();
-            info("Respawn request has been sent to the server.");
-        }
-        */
+
+    @EventHandler
+    private void onTick(final TickEvent.Pre event) {
+    	if (!active) return;
+        if (mc.player.getHealth() < 1f) mc.player.setHealth(20f);
+        if (fullFood.get() && mc.player.getHungerManager().getFoodLevel() < 20) mc.player.getHungerManager().setFoodLevel(20);
     }
     
     @EventHandler
     private void onOpenScreen(OpenScreenEvent event) {
         if (event.screen instanceof DeathScreen) {
             event.cancel();
+
             if (!active) {
                 active = true;
                 info("You are now in ghost mode.");
             }
         }
-    }
-    
+    } 
 }
