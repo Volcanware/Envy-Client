47f87541cfe3050a94a98d1e3c0284c7bf451a57
FurnyGo <68079109+FurnyGo@users.noreply.github.com>
2022-06-10 14:52:03 +0400

    ZV fancy mode
    russian З -> Z
    russian В -> V

--- src/main/java/mathax/client/systems/modules/chat/BetterChat.java
+++ src/main/java/mathax/client/systems/modules/chat/BetterChat.java
@@ -354,6 +354,7 @@ public class BetterChat extends Module {
             case Small_CAPS -> message = applySmall(message);
             case UwU -> message = applyUwU(message);
             case Leet -> message = applyLeet(message);
+            case ZV -> message = applyZV(message);
         }
 
         if (prefix.get()) message = getAffix(prefixText.get(), prefixFont.get(), prefixRandom.get()) + message;
@@ -398,7 +399,14 @@ public class BetterChat extends Module {
 
         return sb.toString();
     }
-
+    
+    public String applyZV(String message) {
+        message = message.replace("в", "v");
+        message = message.replace("В", "V");
+        message = message.replace("З", "Z");
+        return message.replace("з", "z");
+    }
+    
     public String applyUwU(String message) {
         message = message.replace("ove", "uv");
         message = message.replace("the", "da");
@@ -497,6 +505,7 @@ public class BetterChat extends Module {
         Small_CAPS("Small CAPS"),
         UwU("UwU"),
         Leet("Leet"),
+        ZV("ZV"),
         None("None");
 
         private final String title;
