0dca113cb12d3b71ac1976b707a9433d4bde9254
Volcan <78641040+Volcan4436@users.noreply.github.com>
2022-12-13 21:31:59 +1100

    :( 5.0

---
+++ src/main/java/mathax/client/systems/modules/world/Fucker.java
@@ -0,0 +1,44 @@
+package mathax.client.systems.modules.world;
+
+import mathax.client.systems.modules.Categories;
+import mathax.client.systems.modules.Modules;
+import mathax.client.MatHax;
+import mathax.client.systems.modules.Module;
+import mathax.client.systems.modules.render.search.Search;
+import net.minecraft.block.BedBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Items;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+
+public class Fucker extends Module {
+
+
+    public void onTick(PlayerEntity player) {
+        int centerX = player.getBlockPos().getX();
+        int centerY = player.getBlockPos().getY();
+        int centerZ = player.getBlockPos().getZ();
+        World world = player.getEntityWorld();
+        float radius = 5.0f;
+        for (int x = (int) (centerX - radius); x <= (int) (centerX + radius); x++) {
+            for (int y = (int) (centerY - radius); y <= (int) (centerY + radius); y++) {
+                for (int z = (int) (centerZ - radius); z <= (int) (centerZ + radius); z++) {
+                    if ((x - centerX) * (x - centerX) + (y - centerY) * (y - centerY) + (z - centerZ) * (z - centerZ) <= radius * radius) {
+                        BlockState blockState = world.getBlockState(x, y, z);
+                        if (blockState.getBlock() instanceof BedBlock) {
+                            // Mine the Bed
+                            player.swingHand(player.getActiveHand());
+                            world.breakBlock(player.getBlockPos(), true);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    public Fucker() {
+        super(Categories.World, Items.TNT, "Fucker", "Destroys Beds");
+    }
+}
