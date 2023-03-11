e50dcd2b04782c08146bc6ecce719fa014833475
Volcan <78641040+Volcan4436@users.noreply.github.com>
2023-03-11 16:03:15 +1100

    Reach | Done By Wolf

--- src/main/java/mathax/client/systems/modules/ghost/Reach.java
+++ src/main/java/mathax/client/systems/modules/ghost/Reach.java
@@ -1,11 +1,15 @@
 package mathax.client.systems.modules.ghost;
 
+import mathax.client.settings.BoolSetting;
 import mathax.client.settings.DoubleSetting;
 import mathax.client.settings.Setting;
 import mathax.client.settings.SettingGroup;
 import mathax.client.systems.modules.Categories;
 import mathax.client.systems.modules.Module;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.item.SwordItem;
+
 public class Reach extends Module {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
 
@@ -17,13 +21,24 @@ public class Reach extends Module {
         .sliderRange(0, 7.5)
         .build()
     );
+    private final Setting<Boolean> SwordOnly = sgGeneral.add(new BoolSetting.Builder()
+        .name("Sword Only")
+        .description("Only reach if have a sword equipped")
+        .defaultValue(false)
+        .build()
+    );
 
     public Reach() {
         super(Categories.Ghost, Items.COMMAND_BLOCK, "reach", "Gives you super long arms.");
     }
 
+    private boolean isHoldingSword() {
+        ItemStack heldItem = mc.player.getMainHandStack();
+        return heldItem.getItem() instanceof SwordItem;
+    }
+
     public float getReach() {
-        if (!isActive()) return mc.interactionManager.getCurrentGameMode().isCreative() ? 5.0F : 4.5F;
+        if (!isActive() || !isHoldingSword() && SwordOnly.get()) return mc.interactionManager.getCurrentGameMode().isCreative() ? 5.0F : 4.5F;
         return reach.get().floatValue();
     }
 
