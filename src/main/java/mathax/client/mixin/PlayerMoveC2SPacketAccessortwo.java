06ac84fb2973cce2161ef90f0268a1f9af08b728
michael jackson chinese <jc8039047@gmail.com>
2023-04-14 17:20:45 +0100

    maybe fix gradlew build + ItemSucker

--- src/main/java/mathax/client/mixin/PlayerMoveC2SPacketAccessortwo.java
+++ src/main/java/mathax/client/mixin/PlayerMoveC2SPacketAccessortwo.java
@@ -1,17 +1,17 @@
-package mathax.client.mixin;
-
-import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
-import org.spongepowered.asm.mixin.gen.Accessor;
-
-@Mixin(PlayerMoveC2SPacket.class)
-public interface PlayerMoveC2SPacketAccessortwo {
-    @Mutable
-    @Accessor("x")
-    void setX(double x);
-
-    @Mutable
-    @Accessor("z")
-    void setZ(double z);
-}
+//package mathax.client.mixin;
+//
+//import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
+//import org.spongepowered.asm.mixin.Mixin;
+//import org.spongepowered.asm.mixin.Mutable;
+//import org.spongepowered.asm.mixin.gen.Accessor;
+//
+//@Mixin(PlayerMoveC2SPacket.class)
+//public interface PlayerMoveC2SPacketAccessortwo {
+//    @Mutable
+//    @Accessor("x")
+//    void setX(double x);
+//
+//    @Mutable
+//    @Accessor("z")
+//    void setZ(double z);
+//}
