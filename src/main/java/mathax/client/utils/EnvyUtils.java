8089df08e75f92d83508a297837be2e129d54c31
Volcan <78641040+Volcan4436@users.noreply.github.com>
2023-04-15 21:57:56 +1000

    Added EnvyUtils for China Man

---
+++ src/main/java/mathax/client/utils/EnvyUtils.java
@@ -0,0 +1,22 @@
+package mathax.client.utils;
+
+import static mathax.client.MatHax.mc;
+
+public class EnvyUtils {
+    
+    public static void pos() {
+        mc.player.getPos();
+    }
+    public static void velocity() {
+        mc.player.getVelocity();
+    }
+    public static void jump() {
+        mc.player.jump();
+    }
+    public static void fall() {
+        mc.player.setVelocity(mc.player.getVelocity().x, -0.5, mc.player.getVelocity().z);
+    }
+    public static void sprint() {
+        mc.player.setSprinting(true);
+    }
+}
