f685c1bcb691d8f1bf0e81c63de6fedd0736f1c6
michael jackson chinese <jc8039047@gmail.com>
2023-04-15 15:23:04 +0100

    added no cave culling performance improvements

---
+++ src/main/java/mathax/client/systems/modules/render/NoCaveCulling.java
@@ -0,0 +1,26 @@
+package mathax.client.systems.modules.render;
+
+import mathax.client.systems.modules.Categories;
+import mathax.client.systems.modules.Module;
+import net.minecraft.item.Items;
+
+public class NoCaveCulling extends Module {
+    public NoCaveCulling() {
+        super(Categories.Render, Items.BEDROCK, "no-cave-culling", "Disables Minecraft's cave culling algorithm.");
+    }
+
+    @Override
+    public boolean onActivate() {
+        super.onActivate();
+        mc.chunkCullingEnabled = false;
+        mc.worldRenderer.reload();
+        return false;
+    }
+
+    @Override
+    public void onDeactivate() {
+        super.onDeactivate();
+        mc.chunkCullingEnabled = true;
+        mc.worldRenderer.reload();
+    }
+}
