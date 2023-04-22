8d8bd98a21db5e0576d15bb7afb40743d6794e89
Volcan <78641040+Volcan4436@users.noreply.github.com>
2023-04-23 00:01:34 +1000

    Added Base for Experimental Bot Module || Needs a lot of work || What we need to work on: AutoDisconnect, Auto Login, Spam, Minehut Auto Random Server, On Join Commands, Auto Suicide, /msg Spammer, AntiAfk, FightBot (Needs a ton of work), MineFort Auto Server Join, BungeeCord Server Scanner, Port Scanner, Bot Communication, Player Follow, Player Protect, Baritone Commands from Private Messages, Discord Bot Support, Auto Module Toggle (Kill aura when threatened or Jesus when drowning), AntiSpam Bypass (Highly Customizable), Auto Harrass and more

---
+++ src/main/java/mathax/client/systems/modules/experimental/Bot.java
@@ -0,0 +1,13 @@
+package mathax.client.systems.modules.experimental;
+
+import mathax.client.systems.modules.Categories;
+import mathax.client.systems.modules.Module;
+import net.minecraft.item.Items;
+
+public class Bot extends Module {
+
+    public Bot() {
+        super(Categories.Experimental, Items.CRAFTING_TABLE, "Bot", "Experimental Super Customizable Bot");
+    }
+
+}
