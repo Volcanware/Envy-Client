4d4025e0f2e4e905265a4acfa6fd8fbe9411c135
Meesterbouwer123 <meesterbouwer123@gmail.com>
2023-09-21 18:48:58 +0200

    Chatting works again

---
+++ src/main/java/mathax/client/mixin/ChatHudLineMixin.java
@@ -0,0 +1,22 @@
+package mathax.client.mixin;
+
+import mathax.client.mixininterface.IChatHudLine;
+import net.minecraft.client.gui.hud.ChatHudLine;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+
+@Mixin(value = { ChatHudLine.class, ChatHudLine.Visible.class })
+public class ChatHudLineMixin implements IChatHudLine {
+    @Unique
+    private int id;
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(int id) {
+        this.id = id;
+    }
+}
