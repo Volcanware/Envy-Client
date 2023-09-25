8c0106b242f8c2acebc6282f8061120658c100a5
Inn3rStellar <troytrimond@gmail.com>
2023-09-25 13:56:33 -0400

    I am a dev now :D

---
+++ src/main/java/mathax/client/systems/modules/movement/speed/modes/Inn3rstellarSpeed.java
@@ -0,0 +1,27 @@
+package mathax.client.systems.modules.movement.speed.modes;
+
+import mathax.client.eventbus.EventHandler;
+import mathax.client.systems.modules.movement.speed.SpeedMode;
+import mathax.client.systems.modules.movement.speed.SpeedModes;
+import mathax.client.utils.player.PlayerUtils;
+
+public class Inn3rstellarSpeed extends SpeedMode {
+
+    public Inn3rstellarSpeed() {
+        super(SpeedModes.Inn3rstellarSpeed);
+    }
+
+    @EventHandler
+    public boolean onTick() {
+        if(mc.player.isOnGround() && PlayerUtils.isMoving()) {
+
+            mc.player.jump();
+            mc.player.airStrafingSpeed = 0.01f;
+        }
+
+
+     return false;
+    }
+
+}
+
