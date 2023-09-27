f07e78a0d358e13b3ea1b5df4240c999b9674150
Meesterbouwer123 <meesterbouwer123@gmail.com>
2023-09-27 22:02:14 +0200

    got 4 crashes, tomorrow more

--- src/main/java/mathax/client/mixin/SplashTextResourceSupplierMixin.java
+++ src/main/java/mathax/client/mixin/SplashTextResourceSupplierMixin.java
@@ -1,6 +1,7 @@
 package mathax.client.mixin;
 
 import mathax.client.MatHax;
+import net.minecraft.client.gui.screen.SplashTextRenderer;
 import net.minecraft.client.resource.SplashTextResourceSupplier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,7 +16,7 @@ public class SplashTextResourceSupplierMixin {
 
 
     @Inject(method = "get", at = @At("HEAD"), cancellable = true)
-    private void onApply(CallbackInfoReturnable<String> info) {
-        info.setReturnValue(MatHax.getSplashes().get(random.nextInt(MatHax.getSplashes().size())));
+    private void onApply(CallbackInfoReturnable<SplashTextRenderer> info) {
+        info.setReturnValue(new SplashTextRenderer(MatHax.getSplashes().get(random.nextInt(MatHax.getSplashes().size()))));
     }
 }
