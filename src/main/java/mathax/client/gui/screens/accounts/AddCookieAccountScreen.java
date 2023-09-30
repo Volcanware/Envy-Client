ac9e0a031a9af45665a427446b85da7d4a909e82
Meesterbouwer123 <meesterbouwer123@gmail.com>
2023-09-30 13:35:51 +0200

    Make the cookie alt experience a bit less bad

--- src/main/java/mathax/client/gui/screens/accounts/AddCookieAccountScreen.java
+++ src/main/java/mathax/client/gui/screens/accounts/AddCookieAccountScreen.java
@@ -4,9 +4,11 @@ import mathax.client.MatHax;
 import mathax.client.gui.GuiTheme;
 import mathax.client.gui.widgets.containers.WTable;
 import mathax.client.gui.widgets.input.WTextBox;
+import mathax.client.gui.widgets.pressable.WButton;
 import mathax.client.systems.accounts.MicrosoftCookieLogin;
 import mathax.client.systems.accounts.types.MicrosoftAccount;
 import mathax.client.utils.network.MatHaxExecutor;
+import org.lwjgl.util.tinyfd.TinyFileDialogs;
 
 import java.io.File;
 
@@ -29,32 +31,38 @@ public class AddCookieAccountScreen extends AddAccountScreen{
         t.add(theme.label("Path: "));
         WTextBox path = t.add(theme.textBox("")).minWidth(400).expandX().widget();
         path.setFocused(true);
+        WButton browse = t.add(theme.button("Browse")).expandX().widget();
+        browse.action = () -> MatHaxExecutor.EXECUTOR.execute(
+            () -> {
+                String temp = TinyFileDialogs.tinyfd_openFileDialog("Select Cookie alt", path.get(), null, null, false);
+                if (temp != null) {
+                    path.set(temp);
+                }
+            });
         t.row();
 
         // Add
         add = t.add(theme.button("Add")).expandX().widget();
-        add.action = () -> {
-            MatHaxExecutor.EXECUTOR.execute(
-                () -> {
-                    if (!path.get().isEmpty()) {
-                        try {
-                            locked = true;
-                            File file = new File(path.get());
-                            String refreshToken = MicrosoftCookieLogin.getRefreshTokenFromCookie(file);
-                            locked = false;
+        add.action = () -> MatHaxExecutor.EXECUTOR.execute(
+            () -> {
+                if (!path.get().isEmpty()) {
+                    try {
+                        locked = true;
+                        File file = new File(path.get());
+                        String refreshToken = MicrosoftCookieLogin.getRefreshTokenFromCookie(file);
+                        locked = false;
 
-                            if (refreshToken != null) {
-                                MicrosoftAccount account = new MicrosoftAccount(refreshToken);
-                                AccountsScreen.addAccount(this, parent, account);
-                            }
-                        } catch (Exception e) {
-                            exception = e;
-                            locked = false;
+                        if (refreshToken != null) {
+                            MicrosoftAccount account = new MicrosoftAccount(refreshToken);
+                            AccountsScreen.addAccount(this, parent, account);
                         }
+                    } catch (Exception e) {
+                        exception = e;
+                        locked = false;
                     }
                 }
-            );
-        };
+            }
+        );
 
         enterAction = add.action;
     }
